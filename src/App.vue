1b6efb55e318bf315dde5201afff13c2d7a56ec3
Samara Mohsin <Samara Mohsin@DESKTOP-BP4905N>
2023-06-17 01:44:28 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <div v-if="showModal">
+    <!-- <Modal :header="header" :text="text" :header2 ="['sam',4]" theme="sale" @close="toggleModal" /> -->
+    <Modal theme="sale nhi bhej rahe abhi" @close="toggleModal" >
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>ninja gateway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <h1>Sign up to the newsletter</h1>
+    <p>for updates and promo codes!</p>
+    </Modal>
+    
+  </div>
+  <button @click.shift="toggleModal">open modal with shift key</button>
+  <button @click="toggleModal2">open modal </button>
+
+</template>
+
+<script>
+import Modal from './components/Modal'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App!',
+      // header:'Sign up for a gateway!',
+      // text:'Grab your ninja swag for half price!',
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods:{
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModal2(){
+      this.showModal2 = !this.showModal2
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+      <div class="modal" :class="{sale: theme ==='sale'}">
+        <!-- <h1>{{ header }}</h1>
+        <h2>{{ header2 }}</h2>
+        <p>{{ text }}</p> -->
+        <slot>defualt content</slot>
+        <div class="actions">
+          <slot name="links"></slot>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    //props: ['header', 'text','theme','header2'], //parent to child, hum send kr rahe app.vue se aur accept yahan kr rahe hain, its not necessary k jo hum yahan accept kr rahe hain wo parent me ho bhi error nhi aayega
+    props: ['theme'], //parent to child, hum send kr rahe app.vue se aur accept yahan kr rahe hain, its not necessary k jo hum yahan accept kr rahe hain wo parent me ho bhi error nhi aayega
+    
+    methods: {
+      closeModal(){
+          this.$emit('close')
+      }
+    }
+  }
+</script>
+  <style >
+    .modal {
+      width: 400px;
+      padding: 20px;
+      margin: 100px auto;
+      background: white;
+      border-radius: 10px;
+    }
+    .backdrop {
+      top: 0;
+      position: fixed;
+      background: rgba(0,0,0,0.5);
+      width: 100%;
+      height: 100%;
+    }
+    .modal h1{
+        color: aqua;
+        border: none;
+        padding: 0;
+    }
+    .modal p{
+        font-style: normal;
+    }
+    .modal.sale{
+      background: crimson;
+      color: white;
+    }
+    .modal.sale h1{
+      color: white;
+    }
+    .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    /* color: #333; */
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
+    color: white;
+  }
+
+  </style>
